c2906318fd3bb562c86d2a22a20235bc3bb42e45
Toth <ferenc.toth@cgi.com>
2019-11-18 11:34:43 +0100

    Added new constant class
    Added new class PAPSConstants for library sharing trial.

---
+++ src/com/cleverbuilder/PAPSConstants.groovy
@@ -0,0 +1,8 @@
+package com.cleverbuilder
+
+class PAPSConstants {
+    static int SANITY_CHECK_TIMEOUT = 10
+    static String PAPS_DEPLOY_HOST = ' '
+    static String PAPS_DEPLOY_MOBILE_URL = ' '
+    static String PAPS_DEPLOY_FIXNET_URL = ' '
+}
